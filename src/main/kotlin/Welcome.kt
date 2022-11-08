bd105b3399cd691307921d811d3440ebe4d9629a
jrfilocao <bla@email.com>
2022-11-08 20:28:42 +0100

    Add initial files

---
+++ src/main/kotlin/Welcome.kt
@@ -0,0 +1,51 @@
+import csstype.px
+import csstype.rgb
+import react.FC
+import react.Props
+import react.css.css
+import react.dom.html.InputType
+import react.dom.html.ReactHTML.button
+import react.dom.html.ReactHTML.div
+import react.dom.html.ReactHTML.img
+import react.dom.html.ReactHTML.input
+import react.useState
+
+external interface WelcomeProps : Props {
+    var name: String
+}
+
+val Welcome = FC<WelcomeProps> { props ->
+    var name by useState(props.name)
+    div {
+        css {
+            padding = 5.px
+            backgroundColor = rgb(8, 97, 22)
+            color = rgb(56, 246, 137)
+        }
+        +"Hello $name, "
+        +" your name backwards is ${name.reversed()}!!"
+    }
+    input {
+        css {
+            marginTop = 5.px
+            marginBottom = 5.px
+            fontSize = 14.px
+        }
+        type = InputType.text
+        value = name
+        onChange = { event ->
+            name = event.target.value
+        }
+    }
+    div {
+        img {
+            src = "https://placekitten.com/408/287"
+        }
+    }
+    button {
+        onClick = {
+            name = "New name"
+        }
+        +"Change name"
+    }
+}
